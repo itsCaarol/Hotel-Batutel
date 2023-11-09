ac0ed583a59d2ba579e8f6060fc8005a951e3b05
Caroline Araújo <carolineprofissional16@gmail.com>
2023-11-09 00:12:43 -0300

    Protótipo
    primeiro commit

---
+++ Protítipo.rb
@@ -0,0 +1,4 @@
+#esboço do site pelo FIGMA
+#Pegar o máximo de informações e tentar reduzir em duas páginas para facilitar o banco de dados 
+#Usar a logo e colocar um botão de pesquisa 
+#Colocar fundo chamativo com fotos de lugares e quartos 
